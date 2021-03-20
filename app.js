5973c6a98154b92a2125e6930f274953cbd627ef
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-03-20 22:53:09 +0100

    little changes

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const path = require('path')
 const express = require('express')
 
 // routing in express
-const adminData = require('./routes/admin')
+const adminRoute = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+
+const errorController = require('./controllers/error')
 
 const app = express()
 
 // setting the default template engine
+// ejs comes with express but you still need to install it
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
@@ -18,15 +21,10 @@ app.use(express.urlencoded({extended: false})) // this is for body parsing
 // this gives you access to the public folder in the porject
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.route) // page for admin
+app.use('/admin', adminRoute) // page for admin
 app.use(shopRoutes) // page for shops
 
 // page for 404 Page not Found
-app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-  res.status(404).render('404', {
-    pageTitle: '404 Page not found'
-  })
-})
+app.use(errorController.pageNotFound)
 
 app.listen(3000)
