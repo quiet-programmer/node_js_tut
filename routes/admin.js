c961d060d247f6e63eb5819ba7d1351366dc58a4
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-04-15 14:54:08 +0100

    added new admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,35 +1,21 @@
-// const path = require('path')
-
 const express = require('express')
 
 const router = express.Router()
 
-// const rootDir = require('../utils/path')
-
-const products = []
+const adminController = require('../controllers/admin_controller')
 
 // a get request
-router.get('/add-product',(req, res, next) => {
-    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-    res.render('add-product', {
-        pageTitle: 'Add New Products',
-        path: 'productView',
-        activeProduct: true,
-        productCSS: true,
-        formCSS: true,
-        activeProduct: true,
-    })
-})
+router.get('/add-product', adminController.getAddProductPage)
+router.get('/products', adminController.getAdminProductsPage)
+
   
 // a post request
-router.post('/add-product', (req, res, next) => {
-    if(req.body.title === "") {
-        res.redirect('/')
-    } else {
-        products.push({title: req.body.title})
-        res.redirect('/')
-    }
-})
+router.post('/add-product', adminController.postAddProdcts)
 
-exports.route = router
-exports.products = products
+router.get('/edit-product/:productId', adminController.getEidtProductPage)
+
+router.post('/edit-product', adminController.postEditProduct)
+
+router.post('/delete-product', adminController.postDeleteProduct)
+
+module.exports = router
